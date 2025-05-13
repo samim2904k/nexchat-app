916f00fefa5f4cf55f1575bf7e5ccbdfef6f8b31
unknown <samim2904k@gmail.com>
2025-05-13 23:15:30 +0530

    updated the navbar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -28,12 +28,12 @@ const Sidebar = () => {
                 <span>Home</span>
             </Link>
 
-            <Link 
+            {/* <Link 
             to="/friends"
             className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${currentPath=== "/friends" ? "btn-active" : "" }`}>
                 <UsersIcon className='size-5 text-base-content opacity-70'/>
                 <span>Friends</span>
-            </Link>
+            </Link> */}
 
             <Link 
             to="/notifications"
